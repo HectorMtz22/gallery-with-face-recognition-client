2107f9eb6ee91932eb8bbab8556ce591c6c9c07f
Hector Martinez <hec.gre81@gmail.com>
2024-07-14 17:14:21 -0600

    Add transition and color styles

--- src/components/ListOfCategories/index.jsx
+++ src/components/ListOfCategories/index.jsx
@@ -1,19 +1,21 @@
 import { useState, useEffect } from 'react'
 import { getCategories } from '../../services/getCategories'
 import styles from './categories.module.css'
-import { Link } from 'wouter'
+import { Link, useLocation } from 'wouter'
 
 const URI = import.meta.env.VITE_URI_SERVER
 
 const ListOfCategories = () => {
   const [categories, setCategories] = useState([])
+  const [location, setLocation] = useLocation()
+  const categoryActive = location.split('/')[2]
 
   useEffect(() => {
     getCategories()
       .then(response => {
         setCategories(response)
       })
-  })
+  }, [])
 
   return (
     <main className={styles.list}>
@@ -28,8 +30,16 @@ const ListOfCategories = () => {
       </Link>
       {
         categories.map(category => (
-          <Link key={category} className={styles.item} href={`/categories/${category}`}>
-            <img src={`${URI}/categories/${category}`} alt={`Categoría de ${category}`} className={styles.image} />
+          <Link
+            key={category}
+            className={`${styles.item} ${categoryActive === category ? styles.item_active : ''}`}
+            href={`/categories/${category}`}
+          >
+            <img
+              src={`${URI}/categories/${category}`}
+              alt={`Categoría de ${category}`}
+              className={`${styles.image} ${categoryActive === category ? styles.image_active : ''}`}
+            />
             {category}
           </Link>
         ))
