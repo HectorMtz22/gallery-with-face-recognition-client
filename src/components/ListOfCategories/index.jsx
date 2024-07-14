b1bbeffa8770759333ed63d7a1af989521fc12f0
Hector Martinez <hec.gre81@gmail.com>
2024-07-14 16:42:10 -0600

    Add styles and images on the categories

--- src/components/ListOfCategories/index.jsx
+++ src/components/ListOfCategories/index.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from 'react'
 import { getCategories } from '../../services/getCategories'
 import styles from './categories.module.css'
 
+const URI = import.meta.env.VITE_URI_SERVER
+
 const ListOfCategories = () => {
   const [categories, setCategories] = useState([])
 
@@ -15,9 +17,18 @@ const ListOfCategories = () => {
   return (
     <main className={styles.list}>
       <h2>Categorias</h2>
+      <section className={styles.item}>
+        <img
+          src='https://cdn.pixabay.com/photo/2017/01/25/17/35/picture-2008484_1280.png'
+          alt='Todas las categorías'
+          className={styles.image}
+        />
+        Todas
+      </section>
       {
         categories.map(category => (
           <section key={category} className={styles.item}>
+            <img src={`${URI}/categories/${category}`} alt={`Categoría de ${category}`} className={styles.image} />
             {category}
           </section>
         ))
