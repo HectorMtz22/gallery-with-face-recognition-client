66115867136ad2658d21da2c494f52cba07a2a6c
Hector Martinez <hec.gre81@gmail.com>
2024-07-14 17:40:23 -0600

    Add Gallery images

--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import { getPhotos } from '../../services/getPhotos'
+import styles from './gallery.module.css'
 
 const Gallery = () => {
   const [photos, setPhotos] = useState([])
@@ -8,13 +9,18 @@ const Gallery = () => {
       .then(setPhotos)
   }, [])
   return (
-    <div>
+    <main className={styles.flex_gallery}>
       {
         photos.map(photo => (
-          <img key={photo.id} src={photo.url} alt={photo.title} />
+          <section key={photo.filename} className={styles.item}>
+            <img src={photo.url} alt={photo.title} className={styles.photo} />
+            <article className={styles.oculto}>
+              <h2>{photo.classification}</h2>
+            </article>
+          </section>
         ))
       }
-    </div>
+    </main>
   )
 }
 
