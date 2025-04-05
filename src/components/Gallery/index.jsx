98a1295549218ac2ccc44d942c918702c5fe62d5
Hector Martinez <hec.gre81@gmail.com>
2025-04-05 04:52:43 +0000

    Fix name on each photo

--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -1,24 +1,37 @@
 import { useState, useEffect } from 'react'
 import { getPhotos } from '../../services/getPhotos'
 import styles from './gallery.module.css'
+import { getCategories } from '../../services/getCategories'
 
 const Gallery = ({ category, invalidate }) => {
   const [photos, setPhotos] = useState([])
+  const [categories, setCategories] = useState([])
   useEffect(() => {
     getPhotos(category)
       .then(setPhotos)
   }, [category, invalidate])
+
+  useEffect(() => {
+    getCategories()
+      .then(setCategories)
+  }, [])
+
   return (
     <main className={styles.flex_gallery}>
       {
-        photos.map(photo => (
-          <section key={photo.filename} className={styles.item}>
-            <img src={photo.url} alt={photo.title} className={styles.photo} />
-            <article className={styles.oculto}>
-              <h2>{photo.classification}</h2>
-            </article>
-          </section>
-        ))
+        photos.map(photo => {
+          const categoryName = categories
+            .filter(({ id }) => id === photo.classification)
+            .map(({ name }) => name)[0]
+          return (
+            <section key={photo.filename} className={styles.item}>
+              <img src={photo.url} alt={photo.title} className={styles.photo} />
+              <article className={styles.oculto}>
+                <h2>{categoryName}</h2>
+              </article>
+            </section>
+          )
+        })
       }
       {
         photos.length === 0 && (
